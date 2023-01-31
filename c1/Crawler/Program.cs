0088642fe5d76b4afaf7748ad8258645fa8039bf
s20210 <s20210@pjwstk.edu.pl>
2023-01-31 19:27:03 +0100

    commit

--- c1/Crawler/Program.cs
+++ c1/Crawler/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContecnt = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9-]*@[a-z-]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var emailAddresses = regex.Matches(htmlContecnt);
+
+                foreach (var x in emailAddresses)
+                {
+                    Console.WriteLine(x.ToString());
+                }
+
+            }
+            Console.WriteLine("");
         }
     }
 }
